b42eb58a8b5fe8debacf16d096a967a34c88886e
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-10-03 06:57:30 -0700

    task

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - insert node at index
+ * @head: pointer
+ * @idx: index
+ * @n: int
+ * Return: pointer
+ */
+
+listint_t *insert_node(listint_t **head, int n)
+{
+	listint_t *temp, *list;
+
+	list = malloc(sizeof(listint_t));
+
+	temp = *head;
+	if (list == NULL)
+		return (NULL);
+	list->n = n;
+	while (temp)
+	{
+		if (temp->n < n && (temp->next->n >= n || temp->next == NULL))
+		{
+			list->next = temp->next;
+			temp->next = list;
+			return (list);
+		}
+		temp = temp->next;
+	}
+	free(list);
+	return (NULL);
+}
