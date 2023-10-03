1ed25928d018abe8a79971f901eb1d08d6611400
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-10-03 06:37:17 -0700

    task

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print(number % 10)
+    print(number % 10, end='')
     if number >= 0:
         return (number % 10)
     return abs(number % 10) * -1
