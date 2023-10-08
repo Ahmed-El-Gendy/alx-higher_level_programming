0db5eb6e1093f5ec1d8993d910fb0026a4063231
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-10-08 05:36:03 -0700

    task

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    ma = -999999
-    for i in range (len(my_list)):
+    ma = -999999999999999999999999
+    for i in range(len(my_list)):
         if my_list[i] > ma:
             ma = my_list[i]
     return ma
