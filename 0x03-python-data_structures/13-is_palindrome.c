ee9c0c3686274f85bef5747c0fcfd5714588c499
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-10-09 11:44:36 -0700

    task

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - check if list is palindrome
+ * @head: head
+ * Return: int
+ */
+
+int is_palindrome(listint_t **head)
+{
+	int i, n, *arr;
+	const listint_t *current;
+
+	n = 0;
+	arr = malloc(sizeof(int) * 50000);
+	current = *(head);
+	while (current != NULL)
+	{
+		arr[n] = current->n;
+		current = current->next;
+		n++;
+	}
+	for (i = 0; i < n; i++)
+	{
+		if (arr[i] != arr[n - i - 1])
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
