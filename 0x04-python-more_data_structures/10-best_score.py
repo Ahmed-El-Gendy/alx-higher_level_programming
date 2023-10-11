863d7b9dbe9209c5a068068723f8b792671a6055
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-10-11 07:53:55 -0700

    task

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     ma = -1
-    v = None
+    v = "st"
+    if len(a_dictionary) == 0:
+        return None
     for i in a_dictionary:
         if a_dictionary[i] > ma:
             ma = a_dictionary[i]
