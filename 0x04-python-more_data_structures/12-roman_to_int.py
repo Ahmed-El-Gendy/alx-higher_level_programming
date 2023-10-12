deb0f6b14f6c7d3c16a3bd69bc9537710c5cea20
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-10-11 17:24:06 -0700

    task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,9 +4,17 @@ def roman_to_int(roman_string):
     if roman_string == None:
         return 0
     dic = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500,'M':1000}
+    last = 0
     for i in roman_string:
         if i in dic:
-            s += dic[i]
+            if last == 1 and dic[i] > 1:
+                s += dic[i] - 2
+                last = 0
+            elif dic[i] == 1:
+                s += 1
+                last = 1
+            else:
+                s+= dic[i]
         else:
             return 0
     return s
