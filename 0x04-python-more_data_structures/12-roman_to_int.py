b14b99d8a2a894b7751a741d34118e14f7643dfa
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-10-11 17:28:27 -0700

    task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     s = 0
-    if roman_string is None:
+    if roman_string is None and type(roman_string) == str:
         return 0
     dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     last = 0
