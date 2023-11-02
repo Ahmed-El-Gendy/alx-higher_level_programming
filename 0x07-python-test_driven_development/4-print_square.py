077cec44c8cdf0a57012d88c8ec788b5210f9397
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-11-02 02:56:57 -0700

    task

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_square(size):
 
-    if not isinstance(size, (int, float)) or size < 0 or size != int(size):
+    if not isinstance(size, (int, float)) or size != int(size):
         raise TypeError("size must be an integer")
-    if not isinstance(size, (int, float)):
+    if size < 0:
         raise ValueError("size must be >= 0")
 
     size = int(size)
