c1c9435563b90935cc3c24935c4432fb8bcd87cb
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-11-07 14:47:54 -0800

    task

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,4 +3,4 @@ def read_file(filename=""):
     """read file"""
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
-        print(read_data)
+        print(read_data, end='')
