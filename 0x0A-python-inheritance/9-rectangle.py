122634093ef3347a4d1dd0d2c44ead0561792d0a
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-11-06 17:33:52 -0800

    task

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """the class functions"""
+
+    def __init__(self, width, height):
+        """def"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """area"""
+        return self.__height * self.__width
+
+    def __str__(self):
+        """print"""
+        string = "[" + str(self.__class__.__name__)
+        string += "]" + str(self.__width)
+        string += "/" + str(self.__height)
+        return string
