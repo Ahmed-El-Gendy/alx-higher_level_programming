b2b6d92a1edfdae44a708ac6248b3defeaa63204
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-11-07 14:54:33 -0800

    task

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,5 +5,5 @@
 def append_write(filename="", text=""):
     """append to file"""
     with open(filename, 'a', encoding="utf-8") as f:
-        f.write(text)
-        return text.length()
+        return f.write(text)
+
