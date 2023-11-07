300ac25b3b386bbe88355cdd0bcb04619cc6469f
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-11-06 17:48:35 -0800

    task

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""my int class"""
+
+
+class MyInt(int):
+    """invert int"""
+
+    def __fi__(self, value):
+        """== to !="""
+        return self.read != value
+
+    def __se__(self.value):
+        """!= to =="""
+        return self.real == value
