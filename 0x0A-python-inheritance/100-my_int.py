f6c14611ad5c656c25aac33e8f4af42811ac071a
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-11-06 17:53:55 -0800

    task

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,8 +7,8 @@ class MyInt(int):
 
     def __eq__(self, value):
         """== to !="""
-        return not super().__eq__(value)
+        return self.real != value
 
-    def __se__(self, value):
+    def __ne__(self, value):
         """!= to =="""
-        return not super().__ne__(value)
+        return self.real == value
