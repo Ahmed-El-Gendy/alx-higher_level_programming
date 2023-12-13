4f40b1205b88160c0a659fce49918a8a44e23f70
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-12-13 08:44:48 -0800

    task

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,5 +3,5 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres tv_show_genres
 RIGHT JOIN tv_shows tv_shows
 ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.id IS NULL
+WHERE tv_show_genres.show_id IS NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
