9c2a52e20480de5301fe4341e43dcbe8b966fd73
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-12-13 08:19:04 -0800

    task

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- lists all cities contained in the database
+SELECT cities.id, cities.name AS city_name, states.name AS state_name
+FROM cities
+INNTER JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
