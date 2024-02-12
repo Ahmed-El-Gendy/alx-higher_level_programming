bebe2280d5bc6a6ba831e4ceb2ec079148c3605b
Ahmed El Gendy <elgendyahmed753@gmail.com>
2024-02-12 12:31:27 -0800

    task10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 let x = 1;
-function rec(a) {
+function rec (a) {
   if (a === 1) {
     return;
   }
   x *= a;
-  rec (a - 1);
+  rec(a - 1);
 }
 if (!isNaN(process.argv[2])) {
   x = parseInt(process.argv[2]);
-  rec (x - 1);
+  rec(x - 1);
   console.log(x);
 } else {
   console.log(1);
