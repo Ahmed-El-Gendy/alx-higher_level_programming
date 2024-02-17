a24460b4151bca9a80703ffd3728aa7f3fdb13be
Ahmed El Gendy <elgendyahmed753@gmail.com>
2024-02-17 12:31:38 -0800

    task0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,12 +11,11 @@ if __name__ == '__main__':
     a2 = argv[2]
     a3 = argv[3]
     ho = "localhost"
-    db = MySQLdb.connect(host=ho, port=3306, user=a1, password=a2, db=a3)
-
+    db = MySQLdb.connect(host=ho, port=3306, user=a1, passwd=a2, db=a3)
     c = db.cursor()
     c.execute("SELECT * FROM states")
     r = c.fetchall()
     for i in r:
         print(i)
     c.close()
-    dp.close()
+    db.close()
