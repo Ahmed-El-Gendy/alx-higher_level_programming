36fcb75e5acc6253608f1fb87dd34a8a75de637a
Ahmed El Gendy <elgendyahmed753@gmail.com>
2024-02-17 12:48:01 -0800

    task2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""list states"""
+import MySQLdb
+from sys import argv
+
+# The main
+if __name__ == '__main__':
+
+    # comment
+    a1 = argv[1]
+    a2 = argv[2]
+    a3 = argv[3]
+    state_name = argv[4]
+    ho = "localhost"
+    db = MySQLdb.connect(host=ho, port=3306, user=a1, passwd=a2, db=a3)
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name = '{}'\
+            ORDER BY id ASC".format(state_name))
+    r = c.fetchall()
+    for i in r:
+        print(i)
+    c.close()
+    db.close()
