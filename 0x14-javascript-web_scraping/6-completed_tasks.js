1c4fb73d03f9cff25d98dfff2d05b1f63d0eda0e
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-04-27 22:40:16 +0000

    task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, function (err, __res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const todos = JSON.parse(body);
+    const completedTask = {};
+
+    todos.forEach(todo => {
+      if (todo.completed) {
+        if (completedTask[todo.userId]) {
+            completedTask[todo.userId]++;
+        } else {
+            completedTask[todo.userId] = 1;
+        }
+      }
+    });
+
+    console.log(completedTask);
+  }
+});
