a474ef10bd51ffb941d9eecdebfcd43ab738588d
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-04-27 22:12:25 +0000

    task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,6 +2,6 @@
 
 const request = require('request');
 
-request.get('https://swapi-api.alx-tools.com/api/films/'.concat(process.argv[2]), function (err, res, body) {
+request.get('https://swapi-api.alx-tools.com/api/films/'.concat(process.argv[2]), function (__err, __res, body) {
   console.log(JSON.parse(body).title);
 });
