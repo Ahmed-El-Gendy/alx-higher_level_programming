06de9d9fcf8aecda1bc9c5fe0c99bb15c32a3310
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-04-27 22:48:13 +0000

    task 100

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -4,8 +4,8 @@ const request = require('request');
 
 request('https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]), function (__err, __res, body) {
   const characters = JSON.parse(body).characters;
-  for(let i = 0; i < characters.length; i++) {
-    request(characters[i], function(__Err, __Res, Body) {
+  for (let i = 0; i < characters.length; i++) {
+    request(characters[i], function (__Err, __Res, Body) {
       console.log(JSON.parse(Body).name);
     });
   }
