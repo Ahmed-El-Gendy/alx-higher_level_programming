a1bdaa4495f08a0d0832a3245f6b01c03b0e9912
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-04-27 22:25:59 +0000

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 const request = require('request');
-const star_uri = process.argv[2];
+const starUri = process.argv[2];
 let cnt = 0;
 
-request.get(star_uri, function (__err, __res, body) {
+request.get(starUri, function (__err, __res, body) {
   body = JSON.parse(body).results;
   body.forEach(film => {
     const characters = film.characters.map(character => character.split('/')[5]);
